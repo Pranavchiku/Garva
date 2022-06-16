b0e3f2205b1fc899d7033cd864fc1780cb36ef93
Saahil Bhavsar <63697361+XanderWatson@users.noreply.github.com>
2022-06-16 20:59:21 +0530

    Added email smtp settings (#3)
    * Add email settings
    
    * Add email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
