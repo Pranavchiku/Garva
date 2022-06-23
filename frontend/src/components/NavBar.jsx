684986019a0def69a38e75e499c4bce97d75dce1
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-06-23 19:26:20 +0530

    Adds responsiveness to navbar component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,51 +1,36 @@
-import {
-  Button,
-  Flex,
-  Image,
-  Breadcrumb,
-  BreadcrumbItem,
-  Spacer,
-} from "@chakra-ui/react";
+import { Button, Flex, Breadcrumb, BreadcrumbItem, Link } from "@chakra-ui/react";
 
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
 import RegisterEventDrawer from "./RegisterEventDrawer";
 import SubmissionDrawer from "./SubmissionDrawer";
 
-import azadiKaAmritMahotsavLogo from "../assets/logos/akam.png";
-
 export default function NavBar() {
   return (
-    <>
-      <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-        <Breadcrumb
-          className="headerText"
-          spacing="10px"
-          separator={<ChevronRightIcon color="gray.500" />}
-          fontWeight="bold"
-          style={{
-            marginTop: "2vh",
-            marginLeft: "4vw",
-            color: "black",
-          }}
-        >
-          <BreadcrumbItem>
-            <RegisterEventDrawer nav />
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <SubmissionDrawer nav />
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Button variant="ghost">Contact</Button>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Spacer />
-        <Image
-          src={azadiKaAmritMahotsavLogo}
-          className="headerImage"
-          style={{ marginTop: "2vh", marginRight: "1vw" }}
-        />
-      </Flex>
-    </>
+    <Flex display={{ base: "none", md: "flex" }}>
+      <Breadcrumb
+        className="headerText"
+        spacing="10px"
+        separator={<ChevronRightIcon color="gray.500" />}
+        fontWeight="bold"
+        style={{
+          marginTop: "2vh",
+          marginLeft: "4vw",
+          color: "black",
+        }}
+      >
+        <BreadcrumbItem>
+          <RegisterEventDrawer nav />
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <SubmissionDrawer nav />
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Button variant="ghost">
+            <Link href="#footer">Contact</Link>
+          </Button>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </Flex>
   );
 }
