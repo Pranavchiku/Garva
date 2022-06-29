50624a773ca5a3590033a1283d7486dd571a5ff7
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-06-29 23:35:50 +0530

    Updates countdown timer to fetch target date from the backend api

--- frontend/src/utils/eventsUtils.js
+++ frontend/src/utils/eventsUtils.js
@@ -1,5 +1,18 @@
 const baseURL = process.env.REACT_APP_API_URL;
 
+export async function getTargetDate() {
+  const url = baseURL + "events/targetdate/";
+
+  try {
+    const response = await fetch(url, {
+      mode: "cors",
+    });
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export async function getEvents() {
   const url = baseURL + "events/get/";
 
